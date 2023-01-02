a187ee6f36529ad2a60c79949fa96e3ae4416d67
suzi9 <zuzannakre099@gmail.com>
2023-01-02 18:31:40 +0100

    Zmiana długości wierszy

--- Projekt/baza_ksiazek.py
+++ Projekt/baza_ksiazek.py
@@ -26,20 +26,35 @@ class Aplikacja(Tk):
 class Menu(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
-        Label(self, text="BAZA DANYCH KSIĄŻEK", font="Times 35 bold", width= 50, height= 0).pack()
-        Label(self, text="Wybierz jaką akcje chcesz wykonać:", font="Times 20", width= 50, height= 2).pack()
+        Label(self, text="BAZA DANYCH KSIĄŻEK", font="Times 35 bold", 
+              width= 50, height= 0).pack()
+        Label(self, text="Wybierz jaką akcje chcesz wykonać:", font="Times 20",
+              width= 50, height= 2).pack()
 
         #Przyciski
-        Button(self,bg="#D3D3D3", text = "Wyświetl bazę książek", height=3, width=40, font = "Times 15 bold" , cursor="hand2", command= lambda: master.zmiana_okna(Wyswietlanie)).pack()
-        Button(self,bg="#D3D3D3", text = "Dodaj nową książke", height=3, width=40, font = "Times 15 bold" , cursor="hand2", command= lambda: master.zmiana_okna(Dodawanie)).pack()
-        Button(self,bg="#D3D3D3", text = "Usuń książke", height=3, width=40, font = "Times 15 bold" , cursor="hand2", command= lambda: master.zmiana_okna(Usuwanie)).pack()
+        Button(self,bg="#D3D3D3", text = "Wyświetl bazę książek", height=3, 
+                width=40, font = "Times 15 bold" , cursor="hand2", 
+                command= lambda: master.zmiana_okna(Wyswietlanie)).pack()
+
+        Button(self,bg="#D3D3D3", text = "Dodaj nową książke", height=3, 
+                width=40, font = "Times 15 bold" , cursor="hand2", 
+                command= lambda: master.zmiana_okna(Dodawanie)).pack()
+
+        Button(self,bg="#D3D3D3", text = "Usuń książke", height=3, 
+                width=40, font = "Times 15 bold" , cursor="hand2",
+                command= lambda: master.zmiana_okna(Usuwanie)).pack()
+
         # Prycisk który zakańcza pracę programu
-        Button(self,bg="#D3D3D3",text = "Zamknij", height=3, width=40, font = "Times 15 bold" , cursor="hand2", command= lambda: master.destroy()).pack()
+        Button(self,bg="#D3D3D3",text = "Zamknij", height=3, 
+                width=40, font = "Times 15 bold" , cursor="hand2", 
+                command= lambda: master.destroy()).pack()
 
 class Wyswietlanie(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
-        Label(self, text="LISTA WSZYSTKICH KSIĄŻEK W BAZIE", font="Times 20 bold", width= 50, height= 0).pack()
+        
+        Label(self, text="LISTA WSZYSTKICH KSIĄŻEK W BAZIE", font="Times 20 bold", 
+               width= 50, height= 0).pack()
         
         #Wyświetlanie bazy w okienku którym jest możliwość scrollowania
         # read_csv() i .head() pochodzą z modułu pandas
@@ -52,12 +67,16 @@ class Wyswietlanie(Frame):
         wyswietlanie_w_okienku.configure(state='disabled')
         
         #Przyciski
-        Button(self, bg="#D3D3D3",text = "Powrót do strony głównej", height=3, width=40, font = "Times 15 bold" , cursor="hand2", command= lambda: master.zmiana_okna(Menu)).pack()
+        Button(self, bg="#D3D3D3",text = "Powrót do strony głównej", height=3, 
+                width=40, font = "Times 15 bold" , cursor="hand2", 
+                command= lambda: master.zmiana_okna(Menu)).pack()
 
 class Dodawanie(Frame):
     def __init__(self,master):
         Frame.__init__(self, master)
-        Label(self, text="DODAJ NOWĄ KSIĄŻKĘ", font="Times 35 bold", width= 50, height= 0).pack()
+
+        Label(self, text="DODAJ NOWĄ KSIĄŻKĘ", font="Times 35 bold", 
+               width= 50, height= 0).pack()
         
         #Wpowadzanie identyfikatora
         Label(self, text="Podaj identyfikator książki",font="Times 13").pack()
@@ -106,40 +125,59 @@ class Dodawanie(Frame):
                 wszystkie_dane=[zlaczone_dane]
                 zapisac_dane = DataFrame(wszystkie_dane)
 
-                # używam escapechar i quoting aby do pliku .csv zapisywac dane bez cudzyłowów, żeby mi się poprawnie
-                # baza wyświetlała
-                zapisac_dane.to_csv('baza.csv', index=False, mode='a', header=False, escapechar=' ' , quoting=csv.QUOTE_NONE)
+                # używam escapechar i quoting aby do pliku .csv zapisywac dane bez 
+                # cudzysłowów, żeby mi się poprawnie baza wyświetlała
+                zapisac_dane.to_csv('baza.csv', index=False, mode='a', header=False, 
+                                     escapechar=' ' , quoting=csv.QUOTE_NONE)
 
-            # w przeciwnym wypadku dajemy komunikat użytkownikowi że książka o takim ID już istnieje 
+            # w przeciwnym wypadku dajemy komunikat użytkownikowi że książka 
+            # o takim ID już istnieje 
             else:
-                messagebox.showwarning("Uwaga!","Podany identyfikator już istnieje w bazie danych książek.\nProsimy o podanie innego ID!")
+                messagebox.showwarning("Uwaga!",
+                "Podany identyfikator już istnieje w bazie danych książek.\nProsimy o podanie innego ID!")
 
             
         
         # Funkcja do czysczenia pól entry -> tam gdzie się wrowadza dane
-        def wyczysc_wprowadzone_dane(wprowadz_id, wprowadz_tytul, wprowadz_autora, wprowadz_rok):
+        def wyczysc_dane(wprowadz_id, wprowadz_tytul, wprowadz_autora, wprowadz_rok):
             wprowadz_id.delete(0, END)
             wprowadz_tytul.delete(0, END)
             wprowadz_autora.delete(0, END)
             wprowadz_rok.delete(0, END)
 
 
-        # Aby móc dodać dwa przyciski obok siebie tworzę nowy Frame w tym samym okienku i w tym Frame je tam umieszcze   
+        # Aby móc dodać dwa przyciski obok siebie tworzę nowy Frame w tym samym okienku i 
+        # w tym Frame je tam umieszcze   
         top = Frame(self)
         # i wyświetlam go z pozycją u góry
         top.pack(side=TOP)
 
         #Przyciski
-        # Parametr in_= pozwala umieścić widżet w innym kontenerze, o ile argument „in_” jest potomkiem rodzica
-        Button(self,bg="#D3D3D3", text="Zapisz", fg="#3da063",cursor="hand2", width=20, height=3 ,font= "Times 10 bold" , command=lambda:dodaj_ksiazke(wprowadz_id, wprowadz_tytul, wprowadz_autora, wprowadz_rok)).pack(side=LEFT, in_=top)
-        Button(self,bg="#D3D3D3", text="Wyczyść",fg="#4700ff",cursor="hand2" , width=20, height=3 ,font= "Times 10 bold" , command=lambda:wyczysc_wprowadzone_dane(wprowadz_id, wprowadz_tytul, wprowadz_autora, wprowadz_rok)).pack(side=RIGHT, in_=top)
-        Button(self,bg="#D3D3D3", text = "Powrót do strony głównej", height=4, width=40, font = "Times 15 bold" , cursor="hand2", command= lambda: master.zmiana_okna(Menu)).pack()
+        # Parametr in_= pozwala umieścić widżet w innym kontenerze, o ile 
+        # argument „in_” jest potomkiem rodzica
+        Button(self,bg="#D3D3D3", text="Zapisz", fg="#3da063",cursor="hand2", width=20,
+                height=3 ,font= "Times 10 bold" , 
+                command=lambda:dodaj_ksiazke(wprowadz_id, wprowadz_tytul,
+                 wprowadz_autora, wprowadz_rok)).pack(side=LEFT, in_=top)
+
+        Button(self,bg="#D3D3D3", text="Wyczyść",fg="#4700ff",cursor="hand2" , 
+                width=20, height=3 ,font= "Times 10 bold" , 
+                command=lambda:wyczysc_dane(wprowadz_id, wprowadz_tytul,
+                wprowadz_autora, wprowadz_rok)).pack(side=RIGHT, in_=top)
+
+        Button(self,bg="#D3D3D3", text = "Powrót do strony głównej", height=4, 
+                width=40, font = "Times 15 bold" , cursor="hand2", 
+                command= lambda: master.zmiana_okna(Menu)).pack()
 
 class Usuwanie(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
-        Label(self, text="USUŃ KSIĄŻKĘ", font="Times 35 bold", width= 50, height= 0).pack()
-        Label(self, text="Wprowadz ID książki którą chcesz usunąć",font="Times 13").pack()
+
+        Label(self, text="USUŃ KSIĄŻKĘ", font="Times 35 bold",
+               width= 50, height= 0).pack()
+
+        Label(self, text="Wprowadz ID książki którą chcesz usunąć",
+               font="Times 13").pack()
 
         wprowadz_id_ksiazki = Entry(self, width=60)
         # ipady używam do ustawienia wyskości pola o wpisywania czyli entry
@@ -156,11 +194,13 @@ class Usuwanie(Frame):
             # Zmienna która przechowuje całą kolumnę ID
             identyfikator = otwarcie_bazy['ID']
             
-            # Jeżeli podany przez użytkownika identyfikator książki istnieje w wartościach kolumny ID to wykonają się instrukcje dla if
+            # Jeżeli podany przez użytkownika identyfikator książki istnieje 
+            # w wartościach kolumny ID to wykonają się instrukcje dla if
             if id_ksiazki in identyfikator.values:
-                # Wiadomość będzie nam zwracać wartość True jeśli użytkownik kliknie "Tak" i wartość False przy
-                # wyborze przycisku "Nie"
-                g = messagebox.askyesnocancel("Usuwanie książki", "Czy na pewno chcesz kontynuować usuwanie książki z bazy?")
+                # Wiadomość będzie nam zwracać wartość True jeśli użytkownik 
+                # kliknie "Tak" i wartość False przy wyborze przycisku "Nie"
+                g = messagebox.askyesnocancel("Usuwanie książki", 
+                        "Czy na pewno chcesz kontynuować usuwanie książki z bazy?")
                 
                 # Jeżeli użytkownik wciśnie "Tak" to wykona się poniższa intrukcja warunkowa
                 if g == True:
@@ -168,21 +208,33 @@ class Usuwanie(Frame):
                     # jest różne od tego które zostało podane przez użytkownika
                     otwarcie_bazy = otwarcie_bazy[otwarcie_bazy['ID'] != id_ksiazki] 
             else:
-                # Jeżeli podany identyfikator książki nie istnieje w bazie danych to użytkownik dostanie powiadomienie
-                # o tym aby podać istniejący identyfikator
-                messagebox.showwarning("Uwaga!","Podany identyfikator nie istnieje w bazie danych książek.\nProsimy o podanie istniejącego ID!")
+                # Jeżeli podany identyfikator książki nie istnieje w bazie danych to 
+                # użytkownik dostanie powiadomienie o tym aby podać istniejący identyfikator
+                messagebox.showwarning("Uwaga!",
+                "Podany identyfikator nie istnieje w bazie danych książek.\nProsimy o podanie istniejącego ID!")
 
 
             otwarcie_bazy.to_csv('baza.csv', index=False)
         
-        def wyczysc_wprowadzone_dane(wprowadz_id_ksiazki):
+        def wyczysc_dane(wprowadz_id_ksiazki):
             wprowadz_id_ksiazki.delete(0, END)
             
         #Przyciski
-        Button(self, bg="#D3D3D3",text="Usuń", fg="#cf0000", cursor="hand2", width=40, height=3 ,font= "Times 15 bold", command=lambda:usuwanie_ksiazki(wprowadz_id_ksiazki)).pack()
-        Button(self, bg="#D3D3D3",text="Wyczyść", fg="#4700ff",cursor="hand2" , width=40, height=3 ,font= "Times 15 bold" , command=lambda:wyczysc_wprowadzone_dane(wprowadz_id_ksiazki)).pack()
-        Button(self, bg="#D3D3D3",text="Wyświetl bazę po usunięciu książki", cursor="hand2", width=40, height=3 ,font= "Times 15 bold",command= lambda: master.zmiana_okna(Wyswietlanie)).pack()
-        Button(self, bg="#D3D3D3",text = "Powrót do strony głównej", height=3, width=40, font = "Times 15 bold" , cursor="hand2", command= lambda: master.zmiana_okna(Menu)).pack()
+        Button(self, bg="#D3D3D3",text="Usuń", fg="#cf0000", cursor="hand2", 
+                width=40, height=3 ,font= "Times 15 bold",
+                command=lambda:usuwanie_ksiazki(wprowadz_id_ksiazki)).pack()
+
+        Button(self, bg="#D3D3D3",text="Wyczyść", fg="#4700ff",cursor="hand2" , 
+                width=40, height=3 ,font= "Times 15 bold" , 
+                command=lambda:wyczysc_dane(wprowadz_id_ksiazki)).pack()
+
+        Button(self, bg="#D3D3D3",text="Wyświetl bazę po usunięciu książki", cursor="hand2", 
+                width=40, height=3 ,font= "Times 15 bold",
+                command= lambda: master.zmiana_okna(Wyswietlanie)).pack()
+
+        Button(self, bg="#D3D3D3",text = "Powrót do strony głównej", height=3, 
+                width=40, font = "Times 15 bold" , cursor="hand2", 
+                command= lambda: master.zmiana_okna(Menu)).pack()
 
 if __name__ == "__main__":
     baza_ksiazek = Aplikacja()
